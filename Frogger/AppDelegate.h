04c060a643d76245e9365ecef12ac8d66e4c410e
Maxim Shchetynin <git@zeppelinium.de>
2014-09-24 07:35:02 +0200

    Initial Commit

---
+++ Frogger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Frogger
+//
+//  Created by Maxim Shchetynin on 24.09.14.
+//  Copyright (c) 2014 Meine Werke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Frogger/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Frogger
+//
+
+//  Copyright (c) 2014 Meine Werke. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
